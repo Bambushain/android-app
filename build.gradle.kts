89c28619097295758fe14c53dcfe9ba2c88b273d
Imanuel Ulbricht <me@imanuel.dev>
2026-04-16 20:17:40 +0200

    Edit build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-<<<<<<< build.gradle.kts
     id("com.android.application") version "9.1.1" apply false
     id("com.android.library") version "9.1.1" apply false
     id("com.google.dagger.hilt.android") version "2.59.2" apply false
